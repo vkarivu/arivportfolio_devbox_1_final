868a5c53b7da20a471786a17b1104ec9af09d5f8
Ariv Kannagi <vkarivu16@gmail.com>
2024-07-23 07:39:02 +0000

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/arivportfolio_devbox_1_final/", // Replace <repository-name> with your GitHub repository name
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/arivportfolio_devbox_1_final/", // Replace <repository-name> with your GitHub repository name
+// });
