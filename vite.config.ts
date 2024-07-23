3bc83dc0b15804802c7e80338d704619015e7b3a
Ariv Kannagi <vkarivu16@gmail.com>
2024-07-23 07:37:31 +0000

    repo name also changed

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<repository-name>/", // Replace <repository-name> with your GitHub repository name
+  base: "/arivportfolio_devbox_1_final/", // Replace <repository-name> with your GitHub repository name
 });
